6a3cd5604e6ed885273116f6b7ffbc00b3fefc02
Eduardo <webdu15@gmail.com>
2021-02-17 19:01:38 -0300

    insta

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,15 @@
+window.onload = function(){
+    console.log('sem erro')
+
+    var a = document.querySelector('#screen-button')
+    var b = document.querySelector('#circle')
+    a.addEventListener('click',clicar)
+
+    function clicar (){
+    a.className = 'screen-button2'
+    b.className = 'circle2'
+    }
+
+}
+
 
