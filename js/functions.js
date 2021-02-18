f82b060a2e67c3a81381576f6eddc8d59da2ba25
Eduardo <webdu15@gmail.com>
2021-02-17 21:05:27 -0300

    Insta Finished

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,25 @@
 window.onload = function(){
     console.log('sem erro')
 
-    var a = document.querySelector('#screen-button')
-    var b = document.querySelector('#circle')
+    bdy = document.querySelector('#body')
+    a = document.querySelector('#screen-button')
+    b = document.querySelector('#circle')
+    group = document.querySelector('.group')
+    group2 = document.querySelector('.group2')
+    getapp = document.querySelector('.get-app')
+    logo = document.querySelector('.instagram-logo')
+
     a.addEventListener('click',clicar)
 
     function clicar (){
+    bdy.className = 'body2'
     a.className = 'screen-button2'
     b.className = 'circle2'
+    group.style.background = 'none'
+    group2.style.background = 'none'
+    getapp.style.color = "#ccc"
+    logo.className = 'instagram-logo2'
     }
-
 }
 
 
